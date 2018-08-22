fe8fa230cc0af28e0eeb0601e54c1c22205f2f22
akarshsingh9 <akarsh.singh9@gmail.com>
2018-08-22 19:09:13 +0530

    Landing Page UI created. 2 fragments for landing ui and other for search added.

---
+++ app/src/main/java/com/codeinpython/flightsearch/FlightSearch.java
@@ -0,0 +1,25 @@
+package com.codeinpython.flightsearch;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FlightSearch extends Fragment {
+
+
+    public FlightSearch() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_flight_search, container, false);
+    }
+
+}
--- app/src/main/java/com/codeinpython/flightsearch/MainActivity.java
+++ app/src/main/java/com/codeinpython/flightsearch/MainActivity.java
@@ -1,5 +1,6 @@
 package com.codeinpython.flightsearch;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.placeholder,new FlightSearch());
+        transaction.commit();
+
     }
 }
