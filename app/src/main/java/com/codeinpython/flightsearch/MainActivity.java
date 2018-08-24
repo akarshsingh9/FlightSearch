c404525fe97182ed22c974c21742c892f0279b63
akarshsingh9 <akarsh.singh9@gmail.com>
2018-08-24 16:17:52 +0530

    Flights added

--- app/src/main/java/com/codeinpython/flightsearch/MainActivity.java
+++ app/src/main/java/com/codeinpython/flightsearch/MainActivity.java
@@ -88,21 +88,79 @@ public class MainActivity extends AppCompatActivity {
 
     }
     //Database setup method - more cities and flight details can be added to database from here
+    /* Currently you can select from
+      * BOM - Mumbai
+      * MAA - Chennai
+      * DEL - Delhi
+      * BLR - Bangalore
+      * AHM - Ahmedabad
+      * CCU - Kolkata
+      * PAT - Patna
+      */
 void databaseSetup()
         {
             DBHelper db = new DBHelper(this);
             DBAdapter adapter = new DBAdapter(this);
             adapter.openDB();
+            adapter.insert("Air India","25/8/2018","08:00","12:00","4:00","BOM","CCU","3200");
+            adapter.insert("Indigo","25/8/2018","12:00","14:00","2:00","BOM","AHM","2200");
+            adapter.insert("Indigo","25/8/2018","14:00","17:00","3:00","BOM","DEL","2800");
+            adapter.insert("Jet Airways","25/8/2018","09:00","12:00","3:00","BOM","AHM","2000");
+            adapter.insert("Air India","25/8/2018","08:00","10:00","2:00","PAT","CCU","3200");
+            adapter.insert("Indigo","25/8/2018","09:00","12:00","3:00","BOM","CCU","3600");
+            adapter.insert("Air India","25/8/2018","08:00","12:00","4:00","DEL","MAA","4200");
+            adapter.insert("Jet Airways","25/8/2018","12:00","16:00","4:00","DEL","MAA","4000");
+            adapter.insert("Indigo","25/8/2018","09:00","12:00","3:00","DEL","MAA","3600");
+            adapter.insert("Air India","25/8/2018","08:00","12:00","4:00","BOM","CCU","4200");
+            adapter.insert("Jet Airways","25/8/2018","12:00","15:00","3:00","BOM","BLR","4000");
+            adapter.insert("Indigo","25/8/2018","09:00","12:00","3:00","BLR","DEL","3600");
+            adapter.insert("Air India","25/8/2018","08:00","12:00","4:00","BLR","DEL","4000");
+            adapter.insert("Jet Airways","25/8/2018","12:00","15:00","3:00","BLR","DEL","4200");
+
+            adapter.insert("Air India","26/8/2018","08:00","12:00","4:00","BOM","CCU","3200");
+            adapter.insert("Indigo","26/8/2018","12:00","14:00","2:00","BOM","AHM","2200");
+            adapter.insert("Indigo","26/8/2018","14:00","17:00","3:00","BOM","DEL","2800");
+            adapter.insert("Jet Airways","26/8/2018","09:00","12:00","3:00","BOM","AHM","2000");
+            adapter.insert("Air India","26/8/2018","08:00","10:00","2:00","PAT","CCU","3200");
+            adapter.insert("Indigo","26/8/2018","09:00","12:00","3:00","BOM","CCU","3600");
+            adapter.insert("Air India","26/8/2018","08:00","12:00","4:00","DEL","MAA","4200");
+            adapter.insert("Jet Airways","26/8/2018","12:00","16:00","4:00","DEL","MAA","4000");
+            adapter.insert("Indigo","26/8/2018","09:00","12:00","3:00","DEL","MAA","3600");
+            adapter.insert("Air India","26/8/2018","08:00","12:00","4:00","BOM","CCU","4200");
+            adapter.insert("Jet Airways","26/8/2018","12:00","15:00","3:00","BOM","BLR","4000");
+            adapter.insert("Indigo","26/8/2018","09:00","12:00","3:00","BLR","DEL","3600");
+            adapter.insert("Air India","26/8/2018","08:00","12:00","4:00","BLR","DEL","4000");
+            adapter.insert("Jet Airways","26/8/2018","12:00","15:00","3:00","BLR","DEL","4200");
+
+            adapter.insert("Air India","27/8/2018","08:00","12:00","4:00","BOM","CCU","3200");
+            adapter.insert("Indigo","27/8/2018","12:00","14:00","2:00","BOM","AHM","2200");
+            adapter.insert("Indigo","27/8/2018","14:00","17:00","3:00","BOM","DEL","2800");
+            adapter.insert("Jet Airways","27/8/2018","09:00","12:00","3:00","BOM","AHM","2000");
+            adapter.insert("Air India","27/8/2018","08:00","10:00","2:00","PAT","CCU","3200");
+            adapter.insert("Indigo","27/8/2018","09:00","12:00","3:00","BOM","CCU","3600");
+            adapter.insert("Air India","27/8/2018","08:00","12:00","4:00","DEL","MAA","4200");
+            adapter.insert("Jet Airways","27/8/2018","12:00","16:00","4:00","DEL","MAA","4000");
+            adapter.insert("Indigo","27/8/2018","09:00","12:00","3:00","DEL","MAA","3600");
+            adapter.insert("Air India","27/8/2018","08:00","12:00","4:00","BOM","CCU","4200");
+            adapter.insert("Jet Airways","27/8/2018","12:00","15:00","3:00","BOM","BLR","4000");
+            adapter.insert("Indigo","27/8/2018","09:00","12:00","3:00","BLR","DEL","3600");
+            adapter.insert("Air India","27/8/2018","08:00","12:00","4:00","BLR","DEL","4000");
+            adapter.insert("Jet Airways","27/8/2018","12:00","15:00","3:00","BLR","DEL","4200");
+
+
             adapter.insert("Air India","28/8/2018","12:00","14:00","2:00","MAA","BOM","1200");
             adapter.insert("Indigo","28/8/2018","12:00","14:00","2:00","MAA","BOM","1500");
             adapter.insert("Indigo","28/8/2018","12:00","15:00","3:00","MAA","BOM","1800");
+
             adapter.insert("Jet Airways","29/8/2018","12:00","16:00","4:00","MAA","BOM","2000");
             adapter.insert("Jet Airways","29/8/2018","12:00","16:00","4:00","AHM","BOM","2100");
             adapter.insert("Jet Airways","29/8/2018","12:00","16:00","4:00","MAA","CCU","2500");
+            adapter.insert("Jet Airways","29/8/2018","12:00","16:00","4:00","CCU","BOM","3000");
+
             adapter.insert("Air India","30/8/2018","12:00","14:00","2:00","BLR","BOM","1400");
             adapter.insert("Air India","30/8/2018","12:00","14:00","2:00","BLR","CCU","1200");
             adapter.insert("Air India","30/8/2018","08:00","10:00","2:00","BLR","BOM","1000");
-            adapter.insert("Jet Airways","29/8/2018","12:00","16:00","4:00","CCU","BOM","3000");
+
         }
 
 }
